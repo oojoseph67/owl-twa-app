878cd6398701e9b9b94cd2f7cfce14592e6a414a
Joseph <oojoseph67@gmail.com>
2024-10-29 07:24:22 +0100

    chore: backend_url

--- src/utils/config/index.ts
+++ src/utils/config/index.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://owl-twa-backend.onrender.com/api/v1";
+export const BACKEND_URL = "https://owl-twa-backend-o3oe.onrender.com/api/v1";
 export const customUserTelegramId = 1724586309;
